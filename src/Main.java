b7eda36adbc4e2e2b60a7a6036768e1536b95ea8
Nurhaiza Harun <haiza@Nurhaizas-MacBook-Air.local>
2022-04-14 11:36:27 +0800

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,15 @@
+import java.lang.reflect.AccessibleObject;
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        BankAccount haizaacc = new BankAccount();
+        haizaacc.withdrawal(900);
+    }
+
+}
+
+
+
